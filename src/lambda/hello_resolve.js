30e0a6c3c1977c694cd98a40241368c15f8e3a34
Keiko Oda <keiko713@gmail.com>
2018-06-13 16:44:16 -0700

    Add a resolve function

---
+++ src/lambda/hello_resolve.js
@@ -0,0 +1,17 @@
+import http from "http";
+
+export function handler(event, context, callback) {
+  let domain = null;
+
+  domain = event.queryStringParameters.domain;
+  console.log(`Checking ${domain}`);
+
+  http.request({ method: "HEAD", host: domain },
+               res => { console.log(`Server header ${res.headers.server}`)});
+
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({domain: domain})
+  });
+}
+}
