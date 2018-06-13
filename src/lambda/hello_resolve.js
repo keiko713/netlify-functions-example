6d8842d5473b5d8644fc7323031cd894e2c63af8
Keiko Oda <keiko713@gmail.com>
2018-06-13 16:52:17 -0700

    Tweak js

--- src/lambda/hello_resolve.js
+++ src/lambda/hello_resolve.js
@@ -1,9 +1,7 @@
 import http from "http";
 
-export function handler(event, context, callback) {
-  let domain = null;
-
-  domain = event.queryStringParameters.domain;
+exports.handler = function(event, context, callback) {
+  const domain = event.queryStringParameters.domain;
   console.log(`Checking ${domain}`);
 
   http.request({ method: "HEAD", host: domain },
@@ -11,6 +9,6 @@ export function handler(event, context, callback) {
 
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({domain: domain})
+    body: `hello ${domain}`
   });
-}
+};
